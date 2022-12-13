3f23713daf142299fa2d8a7b9449bf0905c91050
Rex Kho <rexkho510@gmail.com>
2022-12-13 13:36:24 -0800

    Finished phase 3

--- practice-for-week-15-react-refactoring-class-components-long-practice/src/components/Weather.js
+++ practice-for-week-15-react-refactoring-class-components-long-practice/src/components/Weather.js
@@ -1,36 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { toQueryString } from '../utils';
 
-class Weather extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        weather: null
-      };
-    }
+const Weather = () => {
+
+  const [weather, setWeather] = useState(null);
+ 
+  useEffect(()=>{
+    navigator.geolocation.getCurrentPosition(
+      pollWeather,
+      (err) => console.log(err),
+      { timeout: 10000 }
+    );
+  },[])
+   
     
-    componentDidMount() {
-      navigator.geolocation.getCurrentPosition(
-        this.pollWeather,
-        (err) => console.log(err),
-        { timeout: 10000 }
-      );
-    }
-    
-    pollWeather = async (location) => {
+    const pollWeather = async (location) => {
       let url = 'http://api.openweathermap.org/data/2.5/weather?';
 
-      /* Remember that it's unsafe to expose your API key. (Note that pushing
-      files that include your key to Github will expose your key!) In
-      production, you would definitely save your key in an environment variable,
-      so do that here. Since this project runs in your local environment
-      (localhost), save your key as an environment variable in a .env file in
-      the root directory of your app. You can then access the key here as
-      "process.env.<variable_name>". Make sure to .gitignore your .env file!
-      Also remember to restart your server (i.e., re-run "npm start") whenever
-      you change your .env file. */
-      const apiKey = '???';
-
+      const apiKey = process.env.REACT_APP_WEATHER_API
+      
       const params = {
         lat: location.coords.latitude,
         lon: location.coords.longitude,
@@ -38,31 +26,31 @@ class Weather extends React.Component {
       };
       
       url += toQueryString(params);
-
+      
       const res = await fetch(url);
       if (res.ok) {
-        const weather = await res.json();
-        this.setState({ weather });
+        const weatherData = await res.json();
+        setWeather(weatherData);
       }
       else {
         alert ("Check Weather API key!")
       }
     }
-
-  render() {
-    const weather = this.state.weather;
-    let content = <div className='loading'>loading weather...</div>;
     
-    if (weather) {
-      const temp = (weather.main.temp - 273.15) * 1.8 + 32;
-      content = (
-        <div>
+    
+      
+      let content = <div className='loading'>loading weather...</div>;
+      
+      if (weather) {
+        const temp = (weather.main.temp - 273.15) * 1.8 + 32;
+        content = (
+          <div>
           <p>{weather.name}</p>
           <p>{temp.toFixed(1)} degrees</p>
         </div>
       );
     }
-
+    
     return (
       <section className="weather-section">
         <h1>Weather</h1>
@@ -71,7 +59,16 @@ class Weather extends React.Component {
         </div>
       </section>
     );
-  }
+  
 }
 
 export default Weather;
+/* Remember that it's unsafe to expose your API key. (Note that pushing
+files that include your key to Github will expose your key!) In
+production, you would definitely save your key in an environment variable,
+so do that here. Since this project runs in your local environment
+(localhost), save your key as an environment variable in a .env file in
+the root directory of your app. You can then access the key here as
+"process.env.<variable_name>". Make sure to .gitignore your .env file!
+Also remember to restart your server (i.e., re-run "npm start") whenever
+you change your .env file. */
